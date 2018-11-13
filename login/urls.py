a325ab77abe83cb7729afe11b457ca981c194ade
HH <howard19970613.gmail.com>
2018-11-13 21:03:00 +0800

    爬蟲公告＋顯示

--- login/urls.py
+++ login/urls.py
@@ -6,4 +6,4 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name="index.html")),
     path('course/', views.post),
     re_path('course/(.+)/$', views.courseLink),#正在創造的功能
-]                   #r'^(\.+)/$'
+]
--- login/views.py
+++ login/views.py
@@ -78,7 +78,33 @@ def find_CurrAccID (course_url, id_enter, password):#找出CurrAccID這個有點
 
 
 
-def courseLink(request, enterNum):#進入課程,爬蟲重新登入
-    unit = userData.objects.get(cID = request.session ["user_id"])
-    
-    return HttpResponse("Enter Course!!!!!!")
+def courseLink(request, enterNum):#給課程id, 進入課程, 爬公告
+    unit = userData.objects.get(cID = request.session ["user_id"])#從session 抓你的帳號 比對資料庫然後抓帳號密碼
+    s= requests.session()
+    headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'}
+    login_DataFrom = {'__EVENTTARGET': '',
+        '__EVENTARGUMENT': '', 
+        '__VIEWSTATE': ' /wEPDwUKMjAzODk5NzA3Mg8WAh4EX2N0bAUMYnRuTG9naW5IZWxwFgICAw9kFiYCAQ8WAh4KYmFja2dyb3VuZAUWaW1hZ2VzL3poLVRXL2xvZ2luLmdpZhYMAgEPFgIeBXN0eWxlBRpwb3NpdGlvbjpyZWxhdGl2ZTtsZWZ0OjBweBYCAgEPDxYCHghJbWFnZVVybAUTaW1hZ2VzL3poLVRXL2lkLmdpZmRkAgMPFgIfAgUacG9zaXRpb246cmVsYXRpdmU7bGVmdDowcHhkAgUPFgIfAgUacG9zaXRpb246cmVsYXRpdmU7bGVmdDowcHgWAmYPZBYCAgEPDxYCHwMFGWltYWdlcy96aC1UVy9wYXNzd29yZC5naWZkZAIHDxYCHwIFGnBvc2l0aW9uOnJlbGF0aXZlO2xlZnQ6MHB4ZAIJD2QWCAIBDw8WBh4IQ3NzQ2xhc3MFC21lbnVfdGV4dDAyHgRUZXh0BQ5b5b+Y6KiY5a+G56K8XR4EXyFTQgICZGQCAw8PFgYfBAUQbWVudV90ZXh0MDJfb190dx8FBQ5b55m75YWl6Kqq5piOXR8GAgJkZAIFDw8WBh8EBQttZW51X3RleHQwMh8FBQ5b6Kiq5a6i5Y+D6KeAXR8GAgJkZAIHDw8WCB8EBQttZW51X3RleHQwMh8FBQ5b5Y+D6KeA6Kqy56iLXR8GAgIeB1Zpc2libGVoZGQCCw8PFgIfAwUcaW1hZ2VzL3poLVRXL2xvZ2luIEVudGVyLmpwZxYEHgtvbm1vdXNlb3ZlcgU4amF2YXNjcmlwdDp0aGlzLnNyYz0naW1hZ2VzL3poLVRXL2xvZ2luIEVudGVyX292ZHcuanBnJzseCm9ubW91c2VvdXQFM2phdmFzY3JpcHQ6dGhpcy5zcmM9J2ltYWdlcy96aC1UVy9sb2dpbiBFbnRlci5qcGcnO2QCAw8PFgIfAwUTaW1hZ2VzL3poLVRXL0dCLmdpZmRkAgQPDxYCHwMFE2ltYWdlcy96aC1UVy9Fbi5naWZkZAIGDw8WAh8DBRZpbWFnZXMvemgtVFcvdGl0ZWwuanBnZGQCCA8PFgYfBAULbWVudV90ZXh0MDIfBQUOW+ebuOmXnOmAo+e1kF0fBgICZGQCCg8PFgYfBAULbWVudV90ZXh0MDIfBQUOW+W5s+WPsOS7i+e0uV0fBgICZGQCDA8PFgYfBAULbWVudV90ZXh0MDIfBQUOW+W4uOimi+WVj+mhjF0fBgICZGQCDg8PFgYfBAULbWVudV90ZXh0MDIfBQUOW+mAo+e1oeaIkeWAkV0fBgICZGQCEA8PFggfBAULbWVudV90ZXh0MDIfBQUOW+eUs+iri+W4s+iZn10fBgICHwdoZGQCFA8PFgIfAwUdaW1hZ2VzL3poLVRXL21haW4gcGljdHVyZS5qcGdkZAIWDxYCHwEFH2ltYWdlcy96aC1UVy9sb2dpbiB0ZXh0IHBhbi5qcGdkAhgPDxYCHwMFFWltYWdlcy96aC1UVy9uZXdzLmpwZ2RkAhwPDxYCHwMFGmltYWdlcy96aC1UVy9mcmFtZV90b3AuZ2lmZGQCHg8WAh8BBR9pbWFnZXMvemgtVFcvbG9naW4gdGV4dCBwYW4uanBnZAIgDxYEHgZoZWlnaHQFBTI0MHB4HgNzcmMFFy4uL2xvZ2luX0hlbHBJbmRleC5hc3B4ZAIiDxYCHwEFGGltYWdlcy96aC1UVy9mcmFtZV9SLmdpZmQCJA8PFgIfAwUaaW1hZ2VzL3poLVRXL2ZyYW1lX2Rvdy5naWZkZAIoDxYEHwUFHGVDYW1wdXMgSUlJIHYxLjYuMDkxOTguMDEwNDAfB2dkAi4PDxYCHwMFH2ltYWdlcy96aC1UVy9sb2dvIG9mIDNwcm9iZS5naWZkZBgBBR5fX0NvbnRyb2xzUmVxdWlyZVBvc3RCYWNrS2V5X18WAwUIYnRuTG9naW4FCmJ0bkNoaW5lc2UFCmJ0bkVuZ2xpc2hqzHG9hdaHqyty7OyKa8boh3mpUA==',
+        '__VIEWSTATEGENERATOR': '8B4B7C2A',
+        'txtLoginId': unit.cID,
+        'txtLoginPwd': unit.cPassword,
+        'btnLogin.x' : '42',
+        'btnLogin.y' : '25',}
+    q= s.get("http://ecampus.nqu.edu.tw/eCampus3P/Learn/stu_course_default.aspx?CourseId="+enterNum+"&Role=Student", headers=headers)#用課程連結進去拿到session
+    p= s.post(q.url, headers=headers, data=login_DataFrom)#登入取得coolies跟session
+    stu_announcement= s.get("http://ecampus.nqu.edu.tw/eCampus3P/Learn/stu_announcement_online.aspx")
+    soup = BeautifulSoup(stu_announcement.text, "lxml")
+    announcement_title =[]
+    announcement_time =[]
+    announcement_text =[]
+    announcement_info =[]
+    for i in soup.find_all("span", id=re.compile("_lbCaption")):
+        announcement_title.append(i.string)
+    for i in soup.find_all("span", id=re.compile("_lbBeginDate")):
+        announcement_time.append(i.string)
+    for i in soup.find_all("span", id=re.compile("_lbContent")):
+        announcement_text.append(i.get_text())#因為有些會有在span中會有一些很奇怪的東西不能用string
+    announcement_info= zip(announcement_title,announcement_time,announcement_text)
+    soup.decompose = True
+    soup.clear()
+    return render(request, "courseInfo.html", locals())
